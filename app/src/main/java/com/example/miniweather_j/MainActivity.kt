c63698d619587115c04e27a8165369fc5a4abd5e
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2026-09-02 14:36:26 -0700

    practica terminada

--- app/src/main/java/com/example/miniweather_j/MainActivity.kt
+++ app/src/main/java/com/example/miniweather_j/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.miniweather_j
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvGreeting: TextView
+    private lateinit var tvCity: TextView
+    private lateinit var ivWeather: ImageView
+    private lateinit var tvTemperature: TextView
+    private lateinit var tvWeather: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Inicializar elementos de la UI
+        tvGreeting = findViewById(R.id.tvGreeting)
+        tvCity = findViewById(R.id.tvCity)
+        ivWeather = findViewById(R.id.ivWeather)
+        tvTemperature = findViewById(R.id.tvTemperature)
+        tvWeather = findViewById(R.id.tvWeather)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
